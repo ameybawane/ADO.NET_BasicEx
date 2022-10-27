9b2fe1ea0315f41f0f1b4617956371ba07781689
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2022-10-27 21:52:59 +0530

    Add files via upload

---
+++ ADODemo/ADODemo/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace ADODemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data source=IN-LT-SPARE09\\SQL2019; database=Sample; integrated security=SSPI");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tblDevice", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
